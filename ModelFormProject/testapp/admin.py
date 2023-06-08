ea5a6e4289e553aaff38428c161c83e63da076c9
pdjango1994 <87570544+pdjango1994@users.noreply.github.com>
2023-06-08 11:45:19 +0530

    Delete ModelFormProject directory

--- ModelFormProject/testapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from testapp.models import Student
-# Register your models here.
-class StudentAdmin(admin.ModelAdmin):
-    list_display=['name','sid','marks','address']
-admin.site.register(Student,StudentAdmin)
--- ModelFormProject/testapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Student(models.Model):
-    name=models.CharField(max_length=15)
-    sid=models.IntegerField()
-    marks=models.IntegerField()
-    address=models.CharField(max_length=30)
